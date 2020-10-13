2911e079260e5cea97eccbe4407366bcced0a324
Luke <luke.belliveau@gmail.com>
2020-10-13 12:54:40 +0200

    roll & select one set of dice

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const rollDie = () => Math.ceil(Math.random() * 6);
+
+const getDiceRoll = () => {
+  const dice = [rollDie(), rollDie(), rollDie(), rollDie(), rollDie()];
+
+  return dice;
+};
+
+const App = () => {
+  let [dice, setDice] = useState([
+    { value: -1, isSelected: false },
+    { value: -1, isSelected: false },
+    { value: -1, isSelected: false },
+    { value: -1, isSelected: false },
+    { value: -1, isSelected: false },
+  ]);
+
+  const rollDice = () => {
+    const diceRoll = getDiceRoll();
+
+    let newDice = [...dice];
+
+    for (var i = 0; i < diceRoll.length; i++) {
+      if (!newDice[i].isSelected) {
+        newDice[i].value = diceRoll[i];
+      }
+    }
+
+    setDice(newDice);
+  };
+
+  const selectDie = (index) => {
+    let newDice = [...dice];
+
+    newDice[index].isSelected = true;
+
+    newDice.sort((die1, die2) => {
+      if (die1.isSelected && !die2.isSelected) return false;
+      return true;
+    });
+
+    setDice(newDice);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {dice.map((die, index) => (
+          <Die
+            value={die.value}
+            isSelected={die.isSelected}
+            select={() => selectDie(index)}
+            key={index}
+          />
+        ))}
+      </div>
+      <button onClick={() => rollDice()}>Roll dice</button>
     </div>
   );
-}
+};
+
+const Die = ({ value, isSelected, select }) => (
+  <div style={styles.die}>
+    <div>{value}</div>
+    <div style={styles.selectButton}>
+      {isSelected ? (
+        <div style={styles.selected}>SELECTED</div>
+      ) : (
+        <button onClick={() => select()}>Select</button>
+      )}
+    </div>
+  </div>
+);
+
+const styles = {
+  die: {
+    "border-bottom": "black 1px solid",
+  },
+  selectButton: {
+    "min-height": "15px",
+    padding: "5px",
+  },
+  selected: {
+    color: "red",
+  },
+};
 
 export default App;
